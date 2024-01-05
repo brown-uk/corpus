e8c705d617576802175f423a11f9f001333c0e4b
Andriy Rysin <arysin@gmail.com>
2024-01-05 13:08:42 -0500

    proces so-so directory too

--- scripts/TaggedStats.groovy
+++ scripts/TaggedStats.groovy
@@ -5,46 +5,52 @@ def SOURCE_URI
 
 def BASE_DIR = new File(SOURCE_URI).parent
 
+def cats = ["good", "so-so"]
 
-println "looking in " + new File("$BASE_DIR/../data/good").absolutePath
-println "Listing untagged files"
+cats.each { cat ->
+    println "looking in " + new File("$BASE_DIR/../data/$cat").absolutePath
+    println "Listing untagged files"
 
-int doneCnt = 0
-int doneSize = 0
-int doneWords = 0
-int todoCnt = 0
-int todoSize = 0
-int todoWords = 0
+    int doneCnt = 0
+    int doneSize = 0
+    int doneWords = 0
+    int todoCnt = 0
+    int todoSize = 0
+    int todoWords = 0
 
-def fileMap = [:]
+    def fileMap = [:]
 
-new File("$BASE_DIR/../data/good").eachFile{ f ->
-    if( ! f.name.endsWith('.txt') )
-        return
+    new File("$BASE_DIR/../data/$cat").eachFile{ f ->
+        if( ! f.name.endsWith('.txt') )
+            return
 
-    def w = f.text =~ /(?ui)[а-яіїєґ][а-яіїєґ'\u2019\u02BC–-]*/
-    int wordCount = w.size()
-        
-    def xmlName = f.name.replaceFirst(/\.txt$/, '.xml')
-    if( new File("$BASE_DIR/../data/disambig", xmlName).isFile() ) {
-        doneCnt++
-        doneSize += f.size()
-        doneWords += wordCount
-        return
+        def w = f.text =~ /(?ui)[а-яіїєґ][а-яіїєґ'\u2019\u02BC–-]*/
+        int wordCount = w.size()
+
+        def xmlName = f.name.replaceFirst(/\.txt$/, '.xml')
+        if( new File("$BASE_DIR/../data/disambig", xmlName).isFile() ) {
+            if( cat == "so-so" ) println "DONE: ${f.name}"
+            
+            doneCnt++
+            doneSize += f.size()
+            doneWords += wordCount
+            return
+        }
+
+        todoCnt++
+        todoSize += f.size()
+        todoWords += wordCount
+
+        fileMap[f.name] = wordCount //[f.size(), wordCount]
     }
 
-    todoCnt++
-    todoSize += f.size()
-    todoWords += wordCount
+    fileMap
+            .toSorted { it -> it.value }
+            .each { k, v ->
+                println "$k, size: $v"
+            }
 
-    fileMap[f.name] = wordCount //[f.size(), wordCount]
+    //println "Done: $doneCnt (words: $doneWords, size: $doneSize), todo: $todoCnt (words: $todoWords, size: $todoSize)"
+    println "Done: $doneCnt (words: $doneWords), todo: $todoCnt (words: $todoWords)"
+
 }
-
-fileMap
-    .toSorted { it -> it.value }
-    .each { k, v ->
-        println "$k, size: $v"
-    }
-
-//println "Done: $doneCnt (words: $doneWords, size: $doneSize), todo: $todoCnt (words: $todoWords, size: $todoSize)"
-println "Done: $doneCnt (words: $doneWords), todo: $todoCnt (words: $todoWords)"
